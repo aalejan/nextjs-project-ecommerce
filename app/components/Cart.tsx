9a915f2b13886e8a6abcbf9336b84aaeaea0b4c2
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2023-05-11 09:55:09 -0400

    added functionality that allows user to update product quantities in the cart

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -2,6 +2,8 @@
 import Image from "next/image";
 import { useCartStore } from "@/store";
 import formatPrice from "@/utils/PriceFormat";
+import { IoAddCircle, IoRemoveCircle } from "react-icons/io5";
+import basket from "@/public/online-purchase.png";
 
 export default function Cart() {
   const cartStore = useCartStore();
@@ -13,7 +15,7 @@ export default function Cart() {
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className='bg-white absolute right-0 top-0 w-1/4 h-screen p-12 overflow-scroll text-gray-700'
+        className='bg-white absolute right-0 top-0 w-1/3 h-screen p-10 overflow-scroll text-gray-700'
       >
         <h1>Shopping List 😂</h1>
         {cartStore.cart.map((item) => (
@@ -27,16 +29,53 @@ export default function Cart() {
             />
             <div>
               <h2>{item.name}</h2>
-              <h2>Quantity : {item.quantity}</h2>
+              <div className='flex gap-2 '>
+                <h2>Quantity : {item.quantity}</h2>
+                <button
+                  onClick={() =>
+                    cartStore.removeProduct({
+                      name: item.name,
+                      id: item.id,
+                      image: item.image,
+                      unit_amount: item.unit_amount,
+                      quantity: item.quantity,
+                    })
+                  }
+                >
+                  <IoRemoveCircle />
+                </button>
+                <button
+                  onClick={() =>
+                    cartStore.addProduct({
+                      name: item.name,
+                      id: item.id,
+                      image: item.image,
+                      unit_amount: item.unit_amount,
+                      quantity: item.quantity,
+                    })
+                  }
+                >
+                  <IoAddCircle />
+                </button>
+              </div>
+
               <p className='text-sm'>
                 {item.unit_amount && formatPrice(item.unit_amount)}
               </p>
             </div>
           </div>
         ))}
-        <button className='py-2 mt-4 bg-teal-700 w-full rounded-md text-white'>
-          Checkout
-        </button>
+        {cartStore.cart.length > 0 && (
+          <button className='py-2 mt-4 bg-teal-700 w-full rounded-md text-white'>
+            Checkout
+          </button>
+        )}
+        {!cartStore.cart.length && (
+          <div className='flex flex-col items-center gap-10 text-2xl font-semibold pt-32 opacity-75'>
+            <h1>You have an empty cart...</h1>
+            <Image src={basket} alt='empty cart' width={200} height={200} />
+          </div>
+        )}
       </div>
     </div>
   );
---
+++ app/components/Hydrate.tsx
@@ -0,0 +1,10 @@
+import { ReactNode, useEffect, useState } from "react";
+
+export default function Hydrate({ children }: { children: ReactNode }) {
+  const [isHydrated, setIsHydrated] = useState(false);
+
+  useEffect(() => {
+    setIsHydrated(true);
+  }, []);
+  return <>{isHydrated ? <>children</> : <h1>Loading...</h1>}</>;
+}
