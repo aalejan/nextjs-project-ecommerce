b8c31b79c06580657ba55a11fe4f1515685379f5
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2023-05-18 15:39:01 -0400

    retrieving user orders in Dashboard and displaying them

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,45 @@
-export default function Dashboard() {
+import { PrismaClient } from "@prisma/client";
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
+const fetchOrders = async () => {
+  const prisma = new PrismaClient();
+  const user = getServerSession(authOptions);
+  if (!user) {
+    return null;
+  }
+  const orders = await prisma.order.findMany({
+    where: { id: user?.user?.id },
+    include: {
+      products: true,
+    },
+  });
+  return orders;
+};
+
+export default async function Dashboard() {
+  const userOrders = await fetchOrders();
+  if (userOrders === null) {
+    return <div>You need to be logged in to view your orders</div>;
+  }
+  if (userOrders.length === 0) {
+    return (
+      <div>
+        <h1>You currently have no orders</h1>
+      </div>
+    );
+  }
   return (
     <div>
-      <h1>Dashboard</h1>
+      <div className='font-medium'>
+        {userOrders.map((order) => (
+          <div key={order.id}>
+            <h2>Order reference: {order.id}</h2>
+            <p>Order placed on: {new Date(order.createdDate).toString()}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ pages/api/orders.ts
@@ -0,0 +1,36 @@
+import Stripe from "stripe";
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    try {
+      //get user
+      const user = await getServerSession(req, res, authOptions);
+      if (!user) {
+        res.status(403).json({ error: "Not logged in" });
+      }
+
+      //get orders for user
+      const orders = await prisma.order.findMany({
+        where: { id: user?.user?.id },
+        include: {
+          products: true,
+        },
+      });
+      res.status(200).json(orders);
+    } catch (error) {
+      res.status(500).json({ message: "Failed to get orders" });
+    }
+  } else {
+    res.setHeader("Allow", "GET");
+    res.status(405).end("Methods not allowed");
+  }
+}
